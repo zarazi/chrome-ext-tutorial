58ef7a53d83bc1381279ec9985671673588f3993
zarazi <zarazi@hotmail.com>
2016-12-18 14:52:35 +0700

    Append sample text to google drive by browserAction

--- eventPage.js
+++ eventPage.js
@@ -2,4 +2,19 @@ console.log('eventpage loaded.');
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     console.log('browserAction clicked!');
+
+    var http = new XMLHttpRequest();
+    var url = "https://script.google.com/macros/s/AKfycbyImFFsA6WPxo9u-aDz52XbMZuJpE87Fl36Fmy8AduydmuVZBo1/exec";
+    var params = "file_name=test&file_content=elephant";
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    http.send(params);
 });
