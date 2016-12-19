d0a515a09dbb476f10c767c008e4efe40bee5428
zarazi <zarazi@hotmail.com>
2016-12-19 10:29:08 +0700

    Send content using form data

--- eventPage.js
+++ eventPage.js
@@ -13,12 +13,10 @@ chrome.contextMenus.create({
     contexts: ['selection']
 });
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    // alert('Sending text to KK-Pedia...\ntitle:'+tab.title+'\ntext:'+info.selectionText);
-    // console.log(info,tab);
     currentFileName = tab.title;
     currentFileContent = info.selectionText;
     chrome.tabs.create({url: chrome.extension.getURL('sendText.html')}, function(tab)  {
-        console.log(tab);
+        console.log('new tab created: '+tab.id);
     });
 });
 function getCurrentFileData() {
@@ -27,14 +25,15 @@ function getCurrentFileData() {
 function sendTextToKKPedia(fileName, fileContent, tabId) {
     currentTabId = tabId;
 
+    var formData = new FormData();
+        formData.append("file_name", fileName);
+        formData.append("file_content", fileContent);
+        console.log('sending data: ', fileName, fileContent);
+
     var http = new XMLHttpRequest();
     var url = "https://script.google.com/macros/s/AKfycbyImFFsA6WPxo9u-aDz52XbMZuJpE87Fl36Fmy8AduydmuVZBo1/exec";
-    var params = "file_name="+fileName+"&file_content="+fileContent;
+
     http.open("POST", url, true);
-
-    //Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
             console.log('done sending text to KKPedia.');
@@ -42,7 +41,7 @@ function sendTextToKKPedia(fileName, fileContent, tabId) {
             if (t && t[0]) t[0].close();
         }
     }
-    http.send(params);
+    http.send(formData);
 }
 
 chrome.runtime.onMessage.addListener(function(message, sender) {
--- sendText.js
+++ sendText.js
@@ -1,20 +1,13 @@
-var fileName = 'FILE_NAME'; //TODO: getting real value from eventPage
-var fileContent = 'FILE_CONTENT'; //TODO: getting real value from eventPage
-
+console.log('initializing..');
 var bg = chrome.extension.getBackgroundPage();
+
 var fileData = bg.getCurrentFileData();
 document.getElementById('fileName').value = fileData.fileName;
 document.getElementById('fileContent').value = fileData.fileContent;
-document.getElementById('sendText').addEventListener('click', function(e){
+
+document.getElementById('sendButton').addEventListener('click', function(e){
     var fileName = document.getElementById('fileName').value;
     var fileContent = document.getElementById('fileContent').value;
-    chrome.tabs.getCurrent(function(tab){
-        console.log(tab.id);
-        bg.sendTextToKKPedia(fileName, fileContent, tab.id);
-    });
-    // chrome.runtime.sendMessage(null, {fileName:fileName, fileContent:fileContent}, {}, function() {
-    //     alert('finish sending.');
-    // });
-    // bg.sendTextToKKPedia(fileName, fileContent);
-})
-console.log(fileData.fileName, fileData.fileContent);
+    console.log('sending data: ', fileName, fileContent);
+    bg.sendTextToKKPedia(fileName, fileContent);
+});
